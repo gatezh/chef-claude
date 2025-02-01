8f8985c8057e7173a2c55be16f0eb1a5365849da
Sergii Gatezh <2880401+gatezh@users.noreply.github.com>
2025-02-01 15:14:26 -0700

    Conditionally render ingredients list and Get recipe section

--- src/Body.jsx
+++ src/Body.jsx
@@ -23,7 +23,24 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientsComponent}</ul>
+
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsComponent}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   );
 }
