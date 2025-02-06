1f972b84f98e9a0a1222f429415ba5c8edc17db9
Sergii Gatezh <2880401+gatezh@users.noreply.github.com>
2025-02-06 10:17:38 -0700

    Organize components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientsList from "./IngredientsList";
-import { /* getRecipeFromChefClaude */ getRecipeFromMistral } from "./ai";
+import { /* getRecipeFromChefClaude */ getRecipeFromMistral } from "../ai";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
